3c6bcb1e4859da3829e072af4fa9039ba5c82458
ashraful-talukder <ashraful.talukder24@gmail.com>
2020-10-20 18:13:12 +0600

    first day - Inversion Control - 2. Code Demo

---
+++ src/springDemo/BaseBallCoach.java
@@ -0,0 +1,9 @@
+package springDemo;
+
+public class BaseBallCoach implements Coach {
+
+	@Override
+	public String getDailyWorkOut() {
+		return "workout on running 10 minutes everyday";
+	}
+}
---
+++ src/springDemo/MyApp.java
@@ -0,0 +1,14 @@
+package springDemo;
+
+public class MyApp {
+
+	public static void main(String[] args) {
+		// create object
+		Coach theCoach = new BaseBallCoach();
+		
+		// use object
+		System.out.println(theCoach.getDailyWorkOut());
+
+	}
+
+}
